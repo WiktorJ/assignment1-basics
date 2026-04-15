7b62e756344545d3696bdbc08ec12c3684b155a9
wiktorj <wiktor.jurasz@gmail.com>
2026-04-15 08:54:12 +0200

    Fixed training script

--- train.sh
+++ train.sh
@@ -3,10 +3,11 @@ uv run python cs336_basics/training.py \
   --train_input_path data/TinyStoriesV2-GPT4-train-encoded.txt.npy \
   --eval_input_path data/TinyStoriesV2-GPT4-valid-encoded.txt.npy \
   --output_path model_training \
-  --max_steps 20 \
+  --max_steps 2000 \
+  --device mps \
   --model_config.vocab_size 10000 \
-  --model_config.context_length 64 \
-  --model_config.d_model 128 \
-  --model_config.num_layers 2 \
-  --model_config.num_heads 2 \
-  --model_config.d_ff 512
+  --model_config.context_length 256 \
+  --model_config.d_model 512 \
+  --model_config.num_layers 8 \
+  --model_config.num_heads 8 \
+  --model_config.d_ff 2048
